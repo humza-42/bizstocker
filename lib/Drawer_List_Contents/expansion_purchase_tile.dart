f124916362b48f5703ee83cd24d39137256ceb79
humza-42 <hamza.shabbir@bitstormsolutions.com>
2026-03-04 10:51:53 +0500

    Added Staggered Animation in drawer

--- lib/Drawer_List_Contents/expansion_purchase_tile.dart
+++ lib/Drawer_List_Contents/expansion_purchase_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class PurchasesExpansionTile extends StatefulWidget {
   const PurchasesExpansionTile({super.key});
@@ -7,8 +8,36 @@ class PurchasesExpansionTile extends StatefulWidget {
   State<PurchasesExpansionTile> createState() => _PurchasesExpansionTileState();
 }
 
-class _PurchasesExpansionTileState extends State<PurchasesExpansionTile> {
+class _PurchasesExpansionTileState extends State<PurchasesExpansionTile>
+    with SingleTickerProviderStateMixin {
   bool _isExpanded = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpanded() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +56,7 @@ class _PurchasesExpansionTileState extends State<PurchasesExpansionTile> {
               _isExpanded ? Icons.expand_less : Icons.expand_more,
               color: Colors.grey,
             ),
-            onPressed: () {
-              setState(() {
-                _isExpanded = !_isExpanded;
-              });
-            },
+            onPressed: _toggleExpanded,
           ),
           onTap: () {
             Navigator.pushNamed(context, '/purchases');
@@ -41,37 +66,75 @@ class _PurchasesExpansionTileState extends State<PurchasesExpansionTile> {
           Column(
             children: [
               ListTile(
-                leading: const Icon(Icons.receipt_long, color: Colors.grey),
-                title: const Text('Orders'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/orders');
-                },
-              ),
+                    leading: const Icon(Icons.receipt_long, color: Colors.grey),
+                    title: const Text('Orders'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/orders');
+                    },
+                  )
+                  .animate()
+                  .fadeIn(duration: 200.ms)
+                  .slideX(
+                    duration: 300.ms,
+                    begin: -0.5,
+                    end: 0.0,
+                    curve: Curves.easeOut,
+                    delay: 50.ms,
+                  ),
               ListTile(
-                leading: const Icon(Icons.replay, color: Colors.grey),
-                title: const Text('Returns'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/returns');
-                },
-              ),
-
+                    leading: const Icon(Icons.replay, color: Colors.grey),
+                    title: const Text('Returns'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/returns');
+                    },
+                  )
+                  .animate()
+                  .fadeIn(duration: 200.ms)
+                  .slideX(
+                    duration: 300.ms,
+                    begin: -0.5,
+                    end: 0.0,
+                    curve: Curves.easeOut,
+                    delay: 100.ms,
+                  ),
               // ListTile(
               //   leading: const Icon(Icons.receipt, color: Colors.grey),
               //   title: const Text('Receive Stock'),
               //   onTap: () {},
-              // ),
+              // ).animate().fadeIn(duration: 200.ms).slideX(
+              //       duration: 300.ms,
+              //       begin: -0.5,
+              //       end: 0.0,
+              //       curve: Curves.easeOut,
+              //       delay: 150.ms,
+              //     ),
               // ListTile(
               //   leading: const Icon(Icons.factory, color: Colors.grey),
               //   title: const Text('Raw Materials'),
               //   onTap: () {},
-              // ),
+              // ).animate().fadeIn(duration: 200.ms).slideX(
+              //       duration: 300.ms,
+              //       begin: -0.5,
+              //       end: 0.0,
+              //       curve: Curves.easeOut,
+              //       delay: 200.ms,
+              //     ),
               ListTile(
-                leading: const Icon(Icons.person, color: Colors.grey),
-                title: const Text('Vendors'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/vendors');
-                },
-              ),
+                    leading: const Icon(Icons.person, color: Colors.grey),
+                    title: const Text('Vendors'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/vendors');
+                    },
+                  )
+                  .animate()
+                  .fadeIn(duration: 200.ms)
+                  .slideX(
+                    duration: 300.ms,
+                    begin: -0.5,
+                    end: 0.0,
+                    curve: Curves.easeOut,
+                    delay: 150.ms,
+                  ),
             ],
           ),
       ],
--- lib/Widgets/custom_drawer.dart
+++ lib/Widgets/custom_drawer.dart
@@ -2,9 +2,34 @@ import '../Drawer_List_Contents/dashboard_item.dart';
 import './drawer_header.dart';
 import '../Drawer_List_Contents/expansion_purchase_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
   const CustomDrawer({super.key});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +46,35 @@ class CustomDrawer extends StatelessWidget {
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
-                  const DashboardItem(),
-                  Divider(),
-                  const PurchasesExpansionTile(),
+                  DashboardItem()
+                      .animate()
+                      .fadeIn(duration: 300.ms)
+                      .slideX(
+                        duration: 500.ms,
+                        begin: -1.0,
+                        end: 0.0,
+                        curve: Curves.easeOut,
+                      ),
+                  Divider()
+                      .animate()
+                      .fadeIn(duration: 300.ms)
+                      .slideX(
+                        duration: 500.ms,
+                        begin: -1.0,
+                        end: 0.0,
+                        curve: Curves.easeOut,
+                        delay: 100.ms,
+                      ),
+                  PurchasesExpansionTile()
+                      .animate()
+                      .fadeIn(duration: 300.ms)
+                      .slideX(
+                        duration: 500.ms,
+                        begin: -1.0,
+                        end: 0.0,
+                        curve: Curves.easeOut,
+                        delay: 200.ms,
+                      ),
                   // const ManufacturingExpansionTile(),
                   // const SalesExpansionTile(),
                   // const InventoryExpansionTile(),
