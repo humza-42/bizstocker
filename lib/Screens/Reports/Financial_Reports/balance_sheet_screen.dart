6551715b32aea222405df507ea0084923bf626e2
humza-42 <hamza.shabbir@bitstormsolutions.com>
2026-03-17 15:01:34 +0500

    Resolved issue of pixel overflow in financial and analysis screens in report's screen

--- lib/Screens/Reports/Financial_Reports/balance_sheet_screen.dart
+++ lib/Screens/Reports/Financial_Reports/balance_sheet_screen.dart
@@ -20,19 +20,23 @@ class BalanceSheetScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          color: Theme.of(context).colorScheme.surfaceContainer,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Balance Sheet',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: 16.0),
-              const ProductionReportCard(),
-            ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            color: Theme.of(context).colorScheme.surfaceContainer,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Balance Sheet',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 16.0),
+                const ProductionReportCard(),
+              ],
+            ),
           ),
         ),
       ),
